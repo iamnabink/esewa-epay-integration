8b09e565e59916000802a33f9215178022d5c3fa
iamnabink <nabrajkhadka431@gmail.com>
2021-05-24 20:56:17 +0545

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:esewa_epay_integration/esewa_epay.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  return runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      home: MyHome(),
+    );
+  }
+}
+
+class MyHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Esewa Epay Webview Integration"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+              onPressed: () {
+                Get.to(() => EsewaEpay());
+              },
+              child: Text("post epay & make payment request"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
